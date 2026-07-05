a1ded55ded4125a0c1e8cc914f83f36e2b983769
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-05 14:15:00 +0200

    chore: refresh flake inputs and LSP env

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Support library and Nix scaffold for manager-flake Rust projects";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor";
+    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk";
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
