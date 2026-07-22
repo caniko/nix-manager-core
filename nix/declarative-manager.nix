1b1e723f69aef971b4cdf78eb5afc519b927660b
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-22 18:27:13 +0200

    feat: support extra runtime packages in declarative manager wrapper
    Add extraRuntimePackages option to declarative-manager.nix so
    modde-manager can include git and procps in its runtime PATH.

--- nix/declarative-manager.nix
+++ nix/declarative-manager.nix
@@ -1,10 +1,17 @@
-{pkgs, managerPackage, managerBinary, config, ...}:
+{
+  pkgs,
+  managerPackage,
+  managerBinary,
+  config,
+  extraRuntimePackages ? [],
+  ...
+}:
 let
   configFile = pkgs.writeText "declarative-manager-config.json" (builtins.toJSON config);
 in
 pkgs.writeShellApplication {
   name = managerBinary;
-  runtimeInputs = [managerPackage];
+  runtimeInputs = [managerPackage] ++ extraRuntimePackages;
   text = ''
     exec ${managerPackage}/bin/${managerBinary} --config ${configFile} "$@"
   '';
