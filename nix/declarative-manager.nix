c2b3a385d87117ab810b61eaaa2a41fc46b72a4f
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-29 06:32:04 +0200

    style: satisfy hosted formatter

--- nix/declarative-manager.nix
+++ nix/declarative-manager.nix
@@ -5,14 +5,13 @@
   config,
   extraRuntimePackages ? [],
   ...
-}:
-let
+}: let
   configFile = pkgs.writeText "declarative-manager-config.json" (builtins.toJSON config);
 in
-pkgs.writeShellApplication {
-  name = managerBinary;
-  runtimeInputs = [managerPackage] ++ extraRuntimePackages;
-  text = ''
-    exec ${managerPackage}/bin/${managerBinary} --config ${configFile} "$@"
-  '';
-}
+  pkgs.writeShellApplication {
+    name = managerBinary;
+    runtimeInputs = [managerPackage] ++ extraRuntimePackages;
+    text = ''
+      exec ${managerPackage}/bin/${managerBinary} --config ${configFile} "$@"
+    '';
+  }
