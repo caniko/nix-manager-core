ef2ed19e4747eac70daaea338e147c972e5d5f75
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:31:56 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Support library and Nix scaffold for manager-flake Rust projects";
 
   inputs = {
-    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=1d9b666489bd9e45411f18b71e5eee320e9a1157";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
